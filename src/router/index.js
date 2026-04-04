efed4cd252bd341f2ffaa9c28045ca3ceb6c67b8
Maximilian Mayer <office@maximilianmayer.at>
2026-04-04 23:03:37 +0200

    setup router

---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import HomeView from "../views/HomeView.vue";
+
+const routes = [{ path: "/", component: HomeView }];
+
+export default createRouter({ history: createWebHistory(), routes });
