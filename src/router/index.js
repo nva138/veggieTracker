7f048ce167bb0f72c3b3b19a67942faee2fa9b26
Maximilian Mayer <office@maximilianmayer.at>
2026-04-09 19:20:51 +0200

    deleted view import fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,11 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import DashboardView from "../views/DashboardView.vue";
 import AddMealView from "../views/AddMealView.vue";
-import FoodDetailView from "../views/FoodDetailView.vue";
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/dashboard", component: DashboardView },
   { path: "/add-meal", component: AddMealView },
-  { path: "/food-detail/:id", component: FoodDetailView },
 ];
 
 export default createRouter({ history: createWebHistory(), routes });
