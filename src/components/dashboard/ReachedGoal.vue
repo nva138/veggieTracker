ca4511a9dfebb06b3d9c3901bfc81f68b84abbb5
Maximilian Mayer <office@maximilianmayer.at>
2026-04-17 22:27:43 +0200

    refactor: ReachedGoal use imported assets

--- src/components/dashboard/ReachedGoal.vue
+++ src/components/dashboard/ReachedGoal.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { useFoodStore } from "../../stores/useFoodStore";
+import barnOpen from "../../assets/barn_open_unlocked.svg";
+import barnClosed from "../../assets/barn_closed_locked.svg";
+
 const store = useFoodStore();
 
 const goalAchievedOrNot = () =>
   store.eaten < store.goal + 50 && store.eaten > store.goal - 50
-    ? "/src/assets/barn_open_unlocked.svg"
-    : "/src/assets/barn_closed_locked.svg";
+    ? barnOpen
+    : barnClosed;
 </script>
 <template>
   <img :src="goalAchievedOrNot()" />
